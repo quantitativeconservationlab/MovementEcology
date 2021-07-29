241721be130fc5d10377a9a249edfb274807b0e3
Jen Cruz <JENCRUZ@boisestate.edu>
2021-07-29 14:02:42 -0600

    working on hr.

--- DefiningTracks.R
+++ DefiningTracks.R
@@ -19,13 +19,12 @@
 install.packages( "tidyverse" ) #actually a collection of packages 
 install.packages( "sp" )
 install.packages( "amt" )
-
+install.packages( "sf" )
 # load packages relevant to this script:
-library( tidyverse ) #easy data manipulation
-library( amt )
 library( sp )
-
-#####################################################################
+library( tidyverse ) #easy data manipulation
+library( amt ) #creating tracks from location data
+library( sf ) #handling spatial data
 ## end of package load ###############
 
 ###################################################################
@@ -43,18 +42,60 @@ workdir <- getwd()
 # Note that the path will be different in yours than mine.#
 datapath <- "Z:/Common/PrairieFalcons/"
 
-#import GPS data
-dataraw <- read.csv( file = paste0( datapath,"ctt_data_export_20210721152221.csv" ),
-                     header = TRUE,
-                     colClasses = c("serial" = "character"))
-#view
-head( dataraw ); dim( dataraw )
+#import GPS data# 
+# records are stored as separate CSV files for each individual
+## We therefore create a function that imports multiple files:
+load_data <- function( path ){
+  myfiles <- dir( path, pattern = '\\.csv', full.names = TRUE )
+  for( i in 1:length(myfiles) ){
+    mydata <- read.csv( file = myfiles[i], strip.white =TRUE, #removes white spaces  
+              header = TRUE, 
+              colClasses = c("serial" = "character") ) 
+   ifelse( i == 1,
+           df <- mydata, 
+           df <- bind_rows(df, mydata) )
+  } 
+  return( df )
+}
+
+#apply function to import all files as list of databases:
+dataraw <- load_data( paste0(datapath, 'allindvs/') )
+
+#import trapping records with details of when radiotrackers were 
+# fitted to the individuals
+records <- read.csv( file = paste0( datapath,"survey_0.csv" ),
+                     #replaces those values with NA, includes column heading
+                     na.strings = c(""," ","NA"), header = TRUE )
+head( records ); dim( records )
+
+#import polygon of the NCA
+NCA_Shape <- st_read("Z:/Common/QCLData/Habitat/NCA/GIS_NCA_IDARNGpgsSampling/BOPNCA_Boundary.shp")
 ##############
 
 #######################################################################
 ######## cleaning data ###############################################
-# Data cleaning is crutial to accurate analysis # 
+# Data cleaning is crucial to accurate analysis # 
+# Trapping records provide info on when individuals were fitted #
+# with transmitters.#
+colnames( records )
+# we keep transmitter id, date and sex
+records <- records %>% dplyr::select( Telemetry.Unit.ID, Sex, StartDay )
+#view
+records
+#convert date to correct format using lubridate
+records$StartDate <- lubridate::mdy_hms( records$Date.and.Time, 
+                                    tz = "MST")
+#add a day so that we can ignore records from the trapping day #
+# and start only with  those from the following day:
+records$StartDate <- records$StartDate + lubridate::days(1)
+#convert to day of year
+records$StartDay <- lubridate::yday( records$StartDate )
+#unit IDs were missing starting number of their serial number #
+# we append those so we can match it to our GPS data
+records$serial <- paste0( '894608001201',records$Telemetry.Unit.ID )
 
+###################################################################
+# Now we clean GPS data
 # GPS units often provide information on the quality of the fixes they #
 # obtained.#
 # These units from Cellular track technologies give us HDOP, VDOP and #
@@ -68,16 +109,18 @@ hist( dataraw$time_to_fix )
 # Remove 2D fixes and fixes where HDOP or VDOP ≥10 #
 # (D’eon and Delparte, 2005; Poessel et al., 2016).#
 # Also those where time to fix > 20min or with 0 satellites
-unique(dataraw$cog )
+
 #start by creating a new dataframe 
 datadf <- dataraw 
 #which columns do we have
 colnames( datadf )
-# Filter to remove inaccurate locations #remove some superfluous columns
+# Filter to remove inaccurate locations 
+#remove some superfluous columns
 datadf <- datadf %>% dplyr::filter( hdop < 10 ) %>%
   dplyr::filter( vdop < 10 ) %>%
   dplyr::filter( time_to_fix <= 20 ) %>% 
   dplyr::filter( nsats > 0 ) %>%
+  dplyr::filter( lat > 0 ) %>% 
   dplyr::select( -inactivity, -geo, -data_voltage, -solar_current, -solar_charge )
 
 #view
@@ -104,12 +147,27 @@ head( datadf ); dim( datadf )
 # check if any data are missing
 all( complete.cases( datadf ) )
 # none so we can move on
-# Our individual IDs are cumbersome so we create a new id column
-datadf$id <- group_indices( datadf, serial )
+
 # we also add month and day of year information using lubridate
 datadf <- datadf %>% 
   mutate( mth = lubridate::month(date),
           jday = lubridate::yday(date) )
+
+# We need to remove records for fixes that were recorded before the #
+# units were fitted to the animals so we append relevant information #
+# from the records dataframe
+datadf <- records %>%  dplyr::select( serial, Sex, StartDay ) %>% 
+  right_join( datadf, by = "serial" )
+#view
+head( datadf ); dim( datadf )
+#filter records to remove those early ones
+datadf <- datadf %>% 
+  group_by( serial ) %>% 
+  filter( jday > StartDay ) %>% ungroup()
+#view
+head( datadf ); dim( datadf )
+# Our individual IDs are cumbersome so we create a new id column
+datadf$id <- group_indices( datadf, serial )
 
 ### Defining coordinate system and projection for the data ######
 # location data were recorded using WGS 84  in lat long #
@@ -127,8 +185,9 @@ crsdata <- sp::CRS( "+init=epsg:4326" )
 # We also want to transform the lat longs to easting and northings #
 # using UTM. For this we need to know what zone we are in. Go: #
 # http://www.dmap.co.uk/utmworld.htm
-crstracks <- sp::CRS( "+proj=utm +zone=12" )
-#
+crstracks <- sp::CRS( "+proj=utm +zone=11" )
+#We convert the NCA shapefile to the same projection as our tracks
+NCA_Shape <- sf::st_transform( NCA_Shape, crstracks )
 # We are now ready to make tracks using atm package. We start with#
 # one individual only. But which one? We first check sample size #
 table( datadf$id )
@@ -143,76 +202,223 @@ ggplot( datadf, aes( x = jday, group = id ) ) +
 # Answer:
 #
 
-
-# Note that we only have 6 records for the 1st individual. #
-# Individual 3 appears to have few records over multiple days #
-# while others have high records over few days (probably collecting) #
-# data in flight mode #
-
-# We start with indiv 3:
+###########################################################################
+###### Creating tracks, calculating step lengths and turning angles ######
+#                     for a single individual                             #
+######################
+# We start by creating a track for a single individual:
 tr.idv <- datadf %>% 
-  #select data for the first individual only
-  dplyr::filter( id == 3 ) %>% 
+  #select data for one individual only
+  dplyr::filter( id == 1 ) %>% 
   # remove duplicate times
   dplyr::filter( !duplicated( ts ) ) %>% 
   #make track. Note you can add additional columns to it
-  amt::make_track(.y = lat, .x = lon, .t = ts, id = id, mth = mth, jday = jday,
-              speed = speed, alt = alt, 
+  amt::make_track(.y = lat, .x = lon, .t = ts, id = id, mth = mth, 
+        jday = jday, sex = Sex, speed = speed, alt = alt, 
               #note that we give it the data CRS to start
               crs = crsdata )
-#we check it
+#check it
 class( tr.idv ); head( tr.idv ); dim( tr.idv )
-# #we project object to UTM:
-tr.idv <- amt::transform_coords( tr.idv, sp::CRS( "+proj=utm +zone=11" ) )
-head( tr.idv)
-# Note, a common mistake when working with spatial data is forgetting to match their projections.#
-# This can introduce significant errors in your analysis. It is particularly common when you #
-# are sourcing your data from multiple locations. #
-
-#### obtaining basic data summaries ###################
-# What datat summaries are useful regardless of your analysis of #
-# interest? 
-# Answer:
-# 
-# We calculate step lengths between locations
-# tr.idv <- tr.idv %>%
-#   mutate( sl_ = amt::step_lengths(.) )
-#view
-summary( tr.idv$sl_ )
-# We need to ensure that sampling rate is approximately regular:
-#view sampling rate:
-amt::summarize_sampling_rate( tr.idv )
-#from here we see that 2hrs mean may be a good resampling option
-tr.slow <- tr.idv %>%  
-  amt::track_resample( rate = hours(2),
-      tolerance = minutes(20) )
-#check
-tr.slow
-# resample subsets the data to the desired sampling frequency #
-# in our case 2 hrs, and adds a 'burst-' column that groups sequence #
-# of relocations within equal sampling rates. This accounts for #
-# gaps in the data due to missing fixes. #
-tr.slow$burst_
-# How many groups or bursts do we have for our individual?
+# A common mistake when working with spatial data is forgetting to #
+# set data to correct projection, which can introduce significant errors #
+# in your analysis #
+# Project object to UTM:
+tr.idv <- amt::transform_coords( tr.idv, crstracks )
+tr.idv
+# Why do we change it to UTM?
+# Answer: 
+#
+# We check our sampling rate:
+tr.idv %>% amt::summarize_sampling_rate()
+# What is it telling us?
 # Answer:
 #
 
-# We can plot step lengths and turning angles for each burst by:
-steps_by_burst( tr.slow )%>% 
-  right_join( tr.slow, by = "burst_" ) %>% 
-  ggplot( . ) +
-  geom_density( aes( y = sl_, fill = as.factor(jday)), alpha = 0.4 ) +
-    #xlab("Step length" ) + xlab("") + 
-  theme_bw( base_size = 19 )  +
-  xlim( 0, 0.004 ) + ylim(0, 7000 )
+# Resample track to high resolution frequency so that we can add a #
+# burst_ id grouping fixes into separte paths #
+# This accounts for gaps in the data due to missing fixes or uneven sampling
+# rates #
+tr.3 <- tr.idv %>%  
+  amt::track_resample( rate = seconds(5),
+                       tolerance = seconds(5) )
+tr.3
+# Convert resulting track to steps, while taking into account the grouping #
+# set by bursts_:
+tr.3 <- amt::steps_by_burst( tr.3 )
+# How many groups or bursts do we have for our individual?
+# Answer:
+#
+# Some analyses require independence of your fix locations. #
+# Temporal autocorrelation of locations leads to underestimation of #
+# home range size and bias in predictions of habitat selection, core area, #
+# and intensity of resource use for those methods that rely on it. #
+# We therefore need to create an additional thinned dataset that removes #
+# autocorrelation in step lengths and turning angles for our individual track#
 
-# plot turning angles:
-steps_by_burst( tr.slow ) %>% 
-  right_join( tr.slow, by = "burst_" ) %>% 
-  gglot( ., aes( x = ta_, fill = as.factor(jday) ) ) +
+# Start by check autocorrelation of track based on direction and turning angles:
+acf( tr.3[,"direction_p"] )
+acf( tr.3[!is.na(tr.3[,'ta_']),'ta_'] )
+# What do these plot tell us? 
+# Answer:
+# 
+# Adjust sampling rate based on results from acf plots above#
+tr.slow <- tr.idv %>%  
+  amt::track_resample( rate = minutes(1),
+                       tolerance = seconds(5) )
+# Recalculate metrics and recheck autocorrelation
+tr.slow <- steps_by_burst( tr.slow )
+acf( tr.slow[,"direction_p"] )
+acf( tr.slow[!is.na(tr.slow[,'ta_']),'ta_'] )
+# What can you see in the new plots?
+# Answer:
+# 
+#How much data did we loose with this resampling strategy?
+tr.slow
+dim(tr.idv)[1] - dim(tr.slow)[1] 
+#Answer:
+#
+# Comment on what this means regarding sample size, etc
+# Answer:
+#
+# We can plot step lengths and turning angles for each burst by:
+tr.slow %>% 
+  ggplot(.) +
+  geom_density( aes( x = sl_, fill = as.factor(burst_)), alpha = 0.4 ) +
+  xlab("Step length" ) + 
+  #ylim( 0, 0.01 ) + xlim(0, 2000 ) +
+  theme_bw( base_size = 19 )  +
+  theme( legend.position = "none" )
+#What does the plot tell us about the step lengths traveled by the individual?
+# Answer:
+
+#
+# Turning angles:
+tr.slow %>% 
+  ggplot( ., aes( x = ta_, y = burst_ ) ) +
   geom_bar(stat="identity") +
   coord_polar() +
   ylab("Turning angle") + xlab("") + 
   theme_bw( base_size = 19)  
+# Is there any evidence of biased movements for this individual?
+# Answer:
+# 
+
+#We now reduce our individual track based to match our reduced steps above 
+tr.red <- tr.slow %>% select( x_= x1_, y_=y1_, t_=t1_ ) %>% 
+  left_join( tr.idv, by = c('x_', "y_", "t_" ) )
+# view
+tr.red
+# Note that the output is now a tibble. We turn it back to a track
+tr.red <- tr.red %>%  
+  amt::make_track(.y = y_, .x = x_, .t = t_, id = id, 
+            mth = mth, jday = jday, speed = speed, alt = alt, 
+            crs = crstracks )
+
+##################################################################
+#### Working with all individuals at the same time:           #####
+########################################################################
+trks <- datadf %>% 
+  #make track. Note you can add additional columns to it
+  amt::make_track(.y = lat, .x = lon, .t = ts, 
+        #define columns that you want to keep
+        id = id, sex = Sex, mth = mth,jday = jday, speed = speed, alt = alt, 
+        #assign crs
+        crs = crsdata )
+
+# Reproject to UTM:
+trks <- amt::transform_coords( trks, crstracks )
+#Group and nest by individual IDs:
+trks <- trks %>%  amt::nest( data = -"id" )
+#view
+trks
+for( i in 1:dim(trks)[1]){
+a <-  steps( trks$data[[i]] ) %>% 
+    mutate( jday = lubridate::yday( t1_ ) ) %>% 
+    group_by( jday ) %>% 
+    summarise( sl_ = log( sum(sl_) ) ) %>% 
+    ggplot(.) + theme_bw(base_size = 17) +
+    labs( title = paste0('individual =', trks$id[i]) ) +
+    geom_line( aes( y = sl_, x = jday))
+print(a)
+}
+
+# How do we know which individuals started migrating North?
+for( i in 1:dim(trks)[1]){
+  a <- as_sf_points( trks$data[[i]] ) %>% 
+    ggplot(.) + theme_bw(base_size = 17) +
+    labs( title = paste0('individual =', trks$id[i]) ) +
+    geom_sf(data = NCA_Shape, inherit.aes = FALSE ) +
+    geom_sf() 
+  print(a)
+} 
+# Which ones have migration paths?
+# Answer:
+#
+# We have multiple types of data including detailed data for flights and #
+# foraging paths, constant data when they are stationary, then a shift #
+# to hourly tracking after they migrate. #
+
+# Sampling rate for each individual by looping through 
+# the data using purrr function map
+sumtrks <- trks %>%  summarize( 
+  map( data, amt::summarize_sampling_rate ) )
+#view
+sumtrks[[1]]
+
+# Add steps by bursts
+trks.all <- trks %>% mutate(
+  steps = map( data, function(x) 
+    x %>%  track_resample( rate = seconds(5), 
+    tolerance = seconds(5)) %>% 
+    steps_by_burst() ) )
+#view
+trks.all
+
+
+# plot autocorrelation for step lengths for all individuals
+par( mfrow = c( 3,3 ) )
+for( i in 1:dim(trks.all)[1] ){
+  #you can modify the lag.max according to your data 
+  idd <- trks.all$id[i]
+  x <- pull( trks.all[["steps"]][[i]], direction_p )
+  x <- x[!is.na(x)]
+  acf( x, lag.max = 300,
+       main = paste0("individual = ",idd ) )
+}
+# What would be a reasonable rate to resample at?
+# Answer:
+# 
+# I choose 25min
+trks.all <- trks.all %>% 
+  mutate(red = map(data, function(x ) x %>%  
+                     track_resample( rate = minutes(25),
+                                     tolerance = minutes(5) ) ) )
+#view
+trks.all
+
+
+
+# We can now unnest the dataframes of interest
+#Starting with the raw data:
+trks.comp <- trks.all %>% select( id, data ) %>% 
+  unnest( cols = data ) 
+head( trks.comp )
+
+# Now the reduced tracks:
+trks.red <- trks.all %>% select( id, red ) %>% 
+  unnest( cols = red ) 
+head( trks.red )
+
+#############################################################################
+# Saving relevant objects and data ---------------------------------
+#save hourly detection dataframe with weather predictors
+# write.csv(x = det_df, 
+#           #ensure that you save it onto your datafolder
+#           file = paste0( datapath, 'stoc_det_df.csv'), 
+#           row.names = FALSE )
+#save workspace in case we need to make changes
+save.image( "TracksWorkspace.RData" )
+
+############### END OF SCRIPT ########################################
 
 ############# end of script  ###########################################
--- Homerange.R
+++ Homerange.R
@@ -21,31 +21,111 @@ library( sp )
 # Clean your workspace to reset your R environment. #
 rm( list = ls() )
 
-# Set working directory. This is the path to your Rstudio folder for this 
-# project. If you are in your correct Rstudio project then it should be:
-getwd()
-# if so then:
-workdir <- getwd()
+# # Set working directory. This is the path to your Rstudio folder for this 
+# # project. If you are in your correct Rstudio project then it should be:
+# getwd()
+# # if so then:
+# workdir <- getwd()
+# 
+# # set path to where you can access your data #
+# # Note that the path will be different in yours than mine.#
+# datapath <- "Z:/Common/PrairieFalcons/"
+# 
+# #import GPS data
+# dataraw <- read.csv( file = paste0( datapath,"ctt_data_export_20210721152221.csv" ),
+#                      header = TRUE,
+#                      colClasses = c("serial" = "character"))
+# #view
+# head( dataraw ); dim( dataraw )
+# load workspace 
+load( "TracksWorkspace.RData" )
+##############   Home range          #####################
 
-# set path to where you can access your data #
-# Note that the path will be different in yours than mine.#
-datapath <- "Z:/Common/PrairieFalcons/"
+# estimate home range using a kernel estimator
+idv_kde <- amt::hr_kde( tr.idv, levels = c(0.5, 0.9) )
+#estimate home range as minimum convex polygon
+idv_mcp <- amt::hr_mcp( tr.idv, levels = c(0.5, 0.9) )
 
-#import GPS data
-dataraw <- read.csv( file = paste0( datapath,"ctt_data_export_20210721152221.csv" ),
-                     header = TRUE,
-                     colClasses = c("serial" = "character"))
 #view
-head( dataraw ); dim( dataraw )
-##############   Home range                      #####################
+par( mfrow = c(1,1))
+plot( idv_kde )
+plot( idv_akde, add.relocations = FALSE, add = TRUE, lty = 3  )
+plot( idv_mcp, add.relocations = FALSE, add = TRUE, lty = 2 )
+# Comment on the difference between the two methods
+# Answer:
+#
+#We can also estimate corresponding areas
+amt::hr_area( idv_kde ); amt::hr_area( idv_mcp )
 
-# use tracking data to create a template raster for the KDE #
-trast <- amt::make_trast( tr.slow )
+#compare methods against reduced data for the same individual:
+red_kde <- amt::hr_kde( tr.red, levels = c(0.5, 0.9) )
+red_mcp <- amt::hr_mcp( tr.red, levels = c(0.5, 0.9) )
+#view
+plot( red_kde, add.relocations = FALSE, add = TRUE, 
+      lwd = 2, col = 'red' )
+plot( red_mcp, add.relocations = TRUE, add = TRUE, 
+      col = 'blue', lwd = 2 )
+#Estimate areas
+amt::hr_area( red_kde ); amt::hr_area( red_mcp )
 
-# estimate home range
-hr_idv <- amt::hr_kde( tr.slow, trast = trast, levels = c(0.5, 0.9) )
-plot( hr_idv)
+# What was the influence of removing temporal autocorrelation on 
+# results from both methods?
+# Answer:
+#
 
+###############################################################
+##### compare methods for all individuals              ########
+##############################################################
+
+# for all data
+hrcomp <- trks.comp %>% amt::nest( data = -"id" ) %>% 
+  mutate(
+    hr_mcp = map(data, ~ hr_mcp(., levels = c(0.5, 0.9)) ),
+    hr_kde = map(data, ~ hr_kde(., levels = c(0.5, 0.9)) ),
+    n = map_int( data, nrow )
+  ) 
+
+#plot homeranges
+hrcomp %>%
+  #choose one method at a time
+  hr_to_sf(  hr_kde, id, n ) %>% 
+ggplot( . ) +
+  theme_bw( base_size = 17 ) + 
+  geom_sf() +
+  facet_wrap( ~id )
+
+
+# for thinned locations:
+hrred <- trks.red %>%  amt::nest( data = -"id" ) %>% 
+  mutate(
+    hr_mcp = map(data, ~ hr_mcp(., levels = c(0.5, 0.9)) ),
+    hr_kde = map(data, ~ hr_kde(., levels = c(0.5, 0.9)) ),
+    #hr_locoh = map(data, ~ hr_locoh(., n = ceiling(sqrt(nrow(.))))),
+    n = map_int( data, nrow )
+  )  
+#view
+hrred 
+
+#plot home ranges
+#select tibble 
+hrred %>%
+  #choose one home range method at a time
+  hr_to_sf( hr_kde, id, n ) %>% 
+  #plot with ggplot
+  ggplot( . ) +
+  theme_bw( base_size = 17 ) + 
+  geom_sf() +
+  #keep separate for each indvidual
+  facet_wrap( ~id )
+
+# remove tracking data and convert to long dataframe
+hrarea <- hrred %>%  select( -data ) %>% 
+  pivot_longer( hr_mcp:hr_kde, names_to = "estimator", 
+                values_to = "hr" )
+
+# Calculate area for each method and unnest list
+hrarea <- hrarea %>%  mutate( hr_area = map( hr, ~hr_area(.)) ) %>% 
+  unnest( cols = hr_area )
 #######################################################################
 ############ Home range overlap ###############################
 # amt currently implements methods reviewed by Fieberg & Kochany (2005) #
