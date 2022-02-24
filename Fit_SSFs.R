63df23e701b237ceaf12739f4eeec2e1b9c8c178
Jen Cruz <JENCRUZ@boisestate.edu>
2022-02-24 12:07:15 -0700

    final version of SSFs.

--- Fit_SSFs.R
+++ Fit_SSFs.R
@@ -38,8 +38,9 @@ trks.steps <- read_rds( "trks.steps30" )
 # load points also so that we can combine data
 trks <- read_rds( "trks.thin" )
 #view
-trks.steps
-trks
+head( trks.steps )
+
+head( trks )
 # #load range for all individuals estimated using ctmm 
 # load( "../ctmm_akde_w.rda" ) 
 # #check that it loaded the object
@@ -79,7 +80,9 @@ sage_cropped[ sage_cropped > 100 ] <- NA
 #Plot sagebrush
 rasterVis::levelplot( sage_cropped )
 
-# to create random points, we start by nesting our data using purr:
+#extract individual ids
+ids <- unique(trks$territory)
+# to create random steps, we start by nesting our data using purr:
 steps_all <- trks.steps %>% nest( data = -"id" )
 #view
 steps_all
@@ -87,13 +90,84 @@ steps_all
 steps_all <- steps_all %>% 
           dplyr::mutate( rnd = lapply( data, function(x){
             amt::random_steps( x ) } ) )
+# Note that by default the random_steps() function fits a tentative #
+# gamma distribution to the observed step lengths and a tentative #
+# von Mises distribution to the observed turn angles. #
+# It then generates random available points by sampling step-lengths #
+# and turn angles from these fitted distributions and combining these #
+# random steps with the starting locations associated with each observed #
+# movement step. #
 
-#now unnest the new dataframes 
+#now unnest the new dataframes to make sure they worked
 stepsdf <- steps_all %>% dplyr::select( id, rnd ) %>% 
   unnest( cols = rnd ) 
 #view
 head( stepsdf );dim( stepsdf )
 
+# Before extracting data we need to turn it to sf object and #
+# change projection to match the raster.
+
+# We start by turning it to sf object, assigning the correct projection
+steps_sf <- sf::st_as_sf( stepsdf, coords = c("x2_", "y2_"), 
+                          crs = crstracks )
+# Note that we use the end of the step coordinates, since we want to 
+# focus on habitat selection, not movement.
+steps_sf
+
+# We then transform the crs:
+steps_trans <- sf::st_transform( steps_sf, st_crs(sage_cropped) )
+
+#extracting with raster we can used the sf object directly, you also 
+# have the choice to use a buffer around each point if you want to increase 
+# your resolution:
+sage_30m <- raster::extract( x = sage_cropped, steps_trans,
+                             method = "simple" )
+
+#check
+sage_30m
+# What proportion of our data are missing values
+sum( is.na( sage_30m ))/ length( sage_30m )
+
+# We append our predictor estimates to the original steps tibble:
+df_all <- cbind( stepsdf, sage_30m )
+# Scale predictors 
+df_all$sage_30m <- scale( df_all$sage_30m )
+#replace missing values with mean, which is 0 after they have been scaled
+df_all$sage_30m[ is.na(df_all$sage_30m) ] <- 0
+# we also assign weights to available points to be much greater than #
+# used points
+df_all$weight <- 1000 ^( 1 - as.integer(df_all$case_ ) )
+#check
+head( df_all )
+#### end data prep #############
+###########################################################################
+##### analyse data  ##########
+
+#amt function doesn't take random effects or weights so we move to a more
+# flexible package. We move straight into a model #
+# that includes random intercepts and slopes as well as a fixed #
+# large variance for the random intercepts, as recommended by Muff #
+# et al. 2019 and weights of 1000 for available points #
+
+# we start by defining the model without running it, which let's us
+# fit the large variance to the random ID intercepts
+m1.struc <- glmmTMB( case_ ~ sage_30m +  
+                   #define random effects
+                   ( 1| step_id_ ) + 
+                   ( 1| id ) + ( 0 + sage_30m | id ), 
+                   family = poisson, data = df_all, 
+                   weights = weight, doFit=FALSE ) 
+
+# fix variance
+m1.struc$parameters$theta[ 1 ] <- log( 1e3 ) 
+# tell it not to change variance
+m1.struc$mapArg <- list( theta = factor( c(NA, 1:2) ) )
+
+#then fit the model
+m1 <- glmmTMB::fitTMB( m1.struc )
+summary( m1 )
+
+###########################################################
 ##### end ######
 ######## how would we partition data into behavioral states prior to #
 # analyses #
@@ -132,8 +206,8 @@ ggplot( trks_all, aes( x = jday, fill = as.factor(wk) ) ) +
 trks_all %>%   
   ggplot(.) +
 #  geom_density( aes( x = sl_, fill = as.factor(wk) ), alpha = 0.6 ) +
-#  geom_density( aes( x = ta_, fill = as.factor(wk) ), alpha = 0.6 ) +
-  geom_density( aes( x = hr, fill = as.factor(wk)), alpha = 0.6 ) +
+  geom_density( aes( x = ta_, fill = as.factor(wk) ), alpha = 0.6 ) +
+#  geom_density( aes( x = hr, fill = as.factor(wk)), alpha = 0.6 ) +
 #  geom_density( aes( x = speed, fill = as.factor(wk) ), alpha = 0.6 ) +
   #geom_histogram( aes( x = sl_, fill = as.factor(wk) ) ) +
   #xlab("Step length" ) + 
@@ -146,26 +220,27 @@ trks_all %>%
 # We plot alternatives selections for step lengths and turning angles
 # to choose parameters that may help to coarsely remove nesting 
 # locations and travelling locations
-ids <- unique(trks_all$territory)
 
-for( i in 1:length( ids ) ){  
+ids <- unique( trks_all$id )
+ters <- unique( trks_all$territory )
+for( i in ters ){  
   #pull observation that you think belong to foraging
-  a <- trks_all %>% dplyr::filter( id == i ) %>% 
+  a <- trks_all %>% dplyr::filter( territory == i ) %>% 
     #filter( ta_ < -0.2 | ta_ > 0.2 ) %>% 
     dplyr::filter( sl_ < 5000 ) %>% 
     dplyr::filter( speed > 1 | speed < 30 ) %>% 
-    dplyr::filter( hr > 6 | hr < 20 ) %>% 
+    #dplyr::filter( hr > 6 | hr < 20 ) %>% 
     sf::st_as_sf(., coords = c("x2_", "y2_"), crs = crstracks )
   #pull all observations for that individual
-  b <- trks_all %>% filter( id == i ) %>% 
+  b <- trks_all %>% filter( territory == i ) %>% 
     sf::st_as_sf(., coords = c("x2_", "y2_"), crs = crstracks )
 #compare
   c  <- ggplot( b ) +
     theme_bw( base_size = 15 ) +
-    labs( title = ids[i] ) +
+    labs( title = i ) +
     theme( legend.position = "none" ) +
     geom_sf() +
-    #geom_sf( data = NCA_Shape, inherit.aes = FALSE, alpha = 0 ) +
+    geom_sf( data = NCA_Shape, inherit.aes = FALSE, alpha = 0 ) +
     geom_sf( data = a, aes(col = as.factor( burst_ ) ), 
                            inherit.aes = FALSE )# +
     #facet_wrap( ~wk )
@@ -178,26 +253,13 @@ for( i in 1:length( ids ) ){
 # to recalculate steps #
 
 ##########################################################################
-
-
-#amt function doesn't take random effects or weights so we move to a more
-# flexible package. Since we already compared different models #
-# for the one individual analysis, we move straight into a model #
-# that includes random intercepts and slopes as well as a fixed #
-# large variance for the random intercepts, as recommended by Muff #
-# et al. 2019 and weights of 1000 for available points #
-
-# we start by defining the model without running it
-mp_30m <- glmmTMB( case_ ~ sage_30m,  
-                   #define random effects
-                   (1|ID) + ( 0 + sage_30m | ID ), 
-                   family = binomial(), data = df_all, 
-                   weights = weight,  ) 
-
-summary( mp_30m )
-###########################################################
 ### Save desired results                                  #
-# we can save the movement model results
+# I save the steps dataframe with extracted raster values so that I 
+# don't have to recreate it when estimating issfs 
+write_rds( trks_all, "trks_all" )
+# I also save the unscaled raster values as a csv
+write.csv( sage_30m, "sage_30m_steps.csv", row.names = FALSE )
+
 #save workspace if in progress
 save.image( 'SSF_results.RData' )
 ############# end of script  ##################################
